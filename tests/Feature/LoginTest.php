41b106e91b2da60afb593088fddffddeb96f6177
Davi Freitas <davifreitaz999@gmail.com>
2025-08-12 17:52:35 -0300

    criando teste unitario de login

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+
+
+
+    public function test_login_user(): void
+    {
+        User::factory()->create([
+            'email' => 'davi@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
+
+
+        $response = $this->postJson('/api/auth/login', [
+            'email' => "davi@gmail.com",
+            "password" => "12345678"
+        ]);
+
+
+        //verifica se retorna a estrutura esperada
+        $response->assertStatus(200)
+            ->assertJsonStructure([
+                'access_token',
+                'token_type',
+                'user',
+            ]);
+    }
+}
