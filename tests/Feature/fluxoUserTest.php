894456e1ee2e5cf91d468d6bb9ae36bfbab74c3a
Davi Freitas <davifreitaz999@gmail.com>
2025-08-12 23:41:28 -0300

    adicionando teste de fluxo do usuario

---
+++ tests/Feature/fluxoUserTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class fluxoUserTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    public function test_fluxo_user(): void
+    {
+        $response = $this->postJson('/api/auth/register', [
+            'name' => 'davi freitas',
+            'email' => 'davifreitas@gmail.com',
+            'password' => '12345678'
+        ]);
+        $response->assertStatus(201);
+
+        $response = $this->postJson('/api/auth/login', [
+            "email" => 'davifreitas@gmail.com',
+            "password" => '12345678'
+        ]);
+        $response->assertStatus(200);
+        $token = $response['access_token'];
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer' . $token,
+        ])->postJson('/api/links', [
+            'original_url' => 'https://www.youtube.com/',
+            'expires_at' => null,
+        ]);
+        $response->assertStatus(201);
+    }
+}
