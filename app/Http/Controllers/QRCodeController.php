41ac911cc7e099069dd5d97ce94b8620b41b7e84
Davi Freitas <davifreitaz999@gmail.com>
2025-08-13 18:18:57 -0300

    foi criado uma nova feature para acessar via qrcode

--- app/Http/Controllers/QRCodeController.php
+++ app/Http/Controllers/QRCodeController.php
@@ -4,8 +4,18 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use LaravelQRCode\Facades\QRCode;
 
 class QRCodeController extends Controller
 {
-    //
+    public function gerarQrCode($slug)
+    {
+
+        $qrcode = QRCode::url(url("/s/{$slug}"))
+            ->setSize(8)
+            ->setMargin(2);
+
+        return response($qrcode->png(), 200)
+            ->header('Content-Type', 'image/png');
+    }
 }
