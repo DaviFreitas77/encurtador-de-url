1b22e64118cffb73718a3dbf449092d081049bb6
Davi Freitas <davifreitaz999@gmail.com>
2025-08-13 20:54:51 -0300

    fix: descomentando rota logout

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('auth')->group(function () {
     Route::post('/register', [RegisterController::class, 'create']);
     Route::post('/login', [LoginController::class, 'login']);
-    // Route::post('/logout', [LoginController::class, 'login']);
+    Route::post('/logout', [LoginController::class, 'logout']);
 });
 
 Route::middleware('auth:sanctum')->group(function () {
